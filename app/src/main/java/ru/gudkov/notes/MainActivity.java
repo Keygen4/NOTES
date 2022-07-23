6c0cb6e0ab1e07be0e2adc8a0cfee2d819a14861
Keygen4 <keygen4@yandex.ru>
2022-07-23 10:23:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/gudkov/notes/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.gudkov.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
